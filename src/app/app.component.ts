dd23f8e0d4502c3dbe659dc8a09848ddc344be1b
pittner <lubos.pittner@posam.sk>
2025-04-12 10:50:17 +0200

    signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   name = 'Angular';
+
+  counterValue = signal(0);
+  counterMultiplied = computed(() => {
+    return this.counterValue() * 2;
+  })
+
+  increment() {
+    this.counterValue.update(value => {
+      return value + 1;
+    })
+  }
+
+  decrement() {
+    this.counterValue.set(this.counterValue() - 1)
+  }
 }
