03d706668f854dfbfd7c79b7824eb3461cab49cd
pittner <lubos.pittner@posam.sk>
2025-04-12 15:47:13 +0200

    refresh

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,41 +6,41 @@ import {DiscussionMessageModel} from '../features/messages/models/discussion-mes
 import {MessageComponent} from '../features/messages/components/message/message.component';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {QuestionModalComponent} from '../question-modal/question-modal.component';
+import {BehaviorSubject, Observable, switchMap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     ReactiveFormsModule,
     SectionHeadlineComponent,
-    MessageComponent
+    MessageComponent,
+    AsyncPipe
   ],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
 
-  messages: DiscussionMessageModel[] | undefined;
+  messages$: Observable<DiscussionMessageModel[] | undefined>;
 
   private messagesService = inject(DiscussionMessagesApiService);
   private ngModal = inject(NgbModal);
 
+  private refreshSubject = new BehaviorSubject(undefined);
+
   constructor() {
-    this.messagesService.getDiscussionMessages()
-      .subscribe({
-        next: (messages) => {
-          this.messages = messages
-        },
-        error: (error) => {
-          alert(error.message)
-        }
-      })
+    this.messages$ = this.refreshSubject
+      .pipe(switchMap(() => {
+        return this.messagesService.getDiscussionMessages();
+      }))
   }
 
   onNewQuestionClick() {
     const modalRef = this.ngModal.open(QuestionModalComponent);
 
-    modalRef.result.then((result) => {
-
-    }, reason => {
+    modalRef.result.then(() => {
+      this.refreshSubject.next(undefined);
+    }, () => {
 
     })
   }
