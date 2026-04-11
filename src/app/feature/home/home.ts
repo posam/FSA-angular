860a4f930572569e44bfa06a62413b77cf563cab
pittner <lubos.pittner@posam.sk>
2026-04-11 12:42:55 +0200

    nacitanie dat

--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SectionContainer } from '../../shared/component/section-container/section-container';
+import { DiscussionMessagesApi } from '../messages/discussion-messages-api';
 
 @Component({
   selector: 'app-home',
   imports: [SectionContainer],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  private api = inject(DiscussionMessagesApi);
+
+  constructor() {
+    this.api.getDiscussionMessages().subscribe((value) => {
+      console.log(value);
+    });
+  }
+}
